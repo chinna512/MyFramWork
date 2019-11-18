032f3be86bd718d17080d5100abd96f542eb70a7
chinna512 <chinnababu.2k14@gmail.com>
2019-11-18 15:55:21 +0530

    Initial Commit

---
+++ MyFramWork/MyFramWork.h
@@ -0,0 +1,19 @@
+//
+//  MyFramWork.h
+//  MyFramWork
+//
+//  Created by Chinnababu Kamanuri on 18/11/19.
+//  Copyright © 2019 Chinnababu Kamanuri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFramWork.
+FOUNDATION_EXPORT double MyFramWorkVersionNumber;
+
+//! Project version string for MyFramWork.
+FOUNDATION_EXPORT const unsigned char MyFramWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramWork/PublicHeader.h>
+
+
